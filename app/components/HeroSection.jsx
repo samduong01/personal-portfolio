ba1eed430555e6abea96731121e00d11dbbe6faa
Sam  Duong <samduong@Sams-MacBook-Pro-37.local>
2023-10-24 21:07:02 -0400

    added back apos

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-500 to-secondary-600">
-              Hello, I'm{" "}
+              Hello, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
@@ -39,7 +39,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            I'm a computer engineering student at Brown University with a passion for the intersection between
+            I&apos;m a computer engineering student at Brown University with a passion for the intersection between
             software and hardware
           </p>
           <div>
