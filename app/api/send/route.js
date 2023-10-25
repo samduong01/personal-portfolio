66333797663ae08ede7e9127564268fcddddfc98
Sam  Duong <samduong@Sams-MacBook-Pro-37.local>
2023-10-25 01:46:21 -0400

    updated email message

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -14,8 +14,7 @@ export async function POST(req, res) {
       subject: subject,
       react: (
         <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
+          <p>Thank you for contacting me I will get back ASAP!</p>
           <p>New message submitted:</p>
           <p>{message}</p>
         </>
