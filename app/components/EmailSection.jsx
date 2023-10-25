9e1ad573bacd1abb65b6bc7f331b300e17dff5c3
Sam  Duong <samduong@Sams-MacBook-Pro-37.local>
2023-10-25 00:45:03 -0400

    email function

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -1,10 +1,49 @@
+"use client";
+import React, { useState } from "react";
+
 import GithubIcon from "public/github-icon.svg";
 import LinkedinIcon from "public/linkedin-icon.svg";
+import EmailIcon from "public/white-email-circle-icon.png";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from 'next/navigation'
 
 
 const EmailSection = () => {
+  const router = useRouter()
+  const [emailSubmitted, setEmailSubmitted] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value,
+    };
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/send";
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSONdata,
+    };
+    console.log("here 1");
+    const response = await fetch(endpoint, options);
+    console.log("here 2");
+
+    const resData = await response.json();
+    console.log("here 3");
+
+    console.log(resData);
+    if (response.status === 200) {
+      console.log("Message sent.");
+      setEmailSubmitted(true);
+    }
+  };
+
   return (
     <section
       id="contact"
@@ -28,7 +67,72 @@ const EmailSection = () => {
           <Link href="https://www.linkedin.com/in/sam-duong/" rel="noopener noreferrer" target="_blank">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
+          <button type="button" onClick={() => router.push('mailto:samduong01@gmail.com')}>
+            <Image src={EmailIcon} alt="Email Icon" width={40} height={40}/>
+          </button>
         </div>
+      </div>
+      <div>
+        {emailSubmitted ? (
+          <p className="text-green-500 text-sm mt-2">
+            Email sent successfully!
+          </p>
+        ) : (
+          <form className="flex flex-col" onSubmit={handleSubmit}>
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="text-white block mb-2 text-sm font-medium"
+              >
+                Your email
+              </label>
+              <input
+                name="email"
+                type="email"
+                id="email"
+                required
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="example@gmail.com"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="subject"
+                className="text-white block text-sm mb-2 font-medium"
+              >
+                Subject
+              </label>
+              <input
+                name="subject"
+                type="text"
+                id="subject"
+                required
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="Just saying hi"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="message"
+                className="text-white block text-sm mb-2 font-medium"
+              >
+                Message
+              </label>
+              <textarea
+                name="message"
+                id="message"
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="Let's talk about..."
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+            >
+              Send Message
+            </button>
+          </form>
+        )}
       </div>
     </section>
   );
