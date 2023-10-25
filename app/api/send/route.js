74c8a7e3686442c17a5b503e31a6f1ccf2dc39d3
Sam  Duong <samduong@Sams-MacBook-Pro-37.local>
2023-10-25 01:36:03 -0400

    fixed email sending

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -10,7 +10,7 @@ export async function POST(req, res) {
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: [fromEmail, email],
+      to: ["samduong01@gmail.com", email],
       subject: subject,
       react: (
         <>
